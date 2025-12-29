f2a988a253e2aeffc2b41cebbe1bd78b38cda0e6
Aesel Reservasi <aeselreservasi@gmail.com>
2025-12-29 21:12:27 +0700

    Update inputData.js

--- inputData.js
+++ inputData.js
@@ -26,34 +26,6 @@
     console.warn("[inputData] user tidak aktif, STOP");
     return;
   }
-  /* ================= RETRY NEXT ================= */
-  function retryClickNext(maxRetry) {
-    var attempt = 0;
-    var timer = setInterval(function () {
-      attempt++;
-
-      var btn = document.getElementById("Next");
-      var canClick = btn && !btn.disabled;
-
-      if (canClick) {
-        clearInterval(timer);
-        console.log("[inputData] Next clicked (attempt " + attempt + ")");
-
-        if (typeof window.fnc_input_check_next === "function") {
-          window.fnc_input_check_next();
-        } else {
-          btn.click();
-        }
-        return;
-      }
-
-      if (attempt >= maxRetry) {
-        clearInterval(timer);
-        console.warn("[inputData] Next gagal setelah " + maxRetry + " percobaan");
-      }
-    }, 500);
-  }
-
   /* ================= FLAGS ================= */
   var autoInput = getFlag("autoInput");
   console.log("[inputData] autoInput =", autoInput, "step =", statusStep);
@@ -119,113 +91,95 @@
 
   /* ================= MAIN LOGIC ================= */
   var exam = localStorage.getItem("exam");
+  inputDataUmum();
 
-  /* ===== STEP 1 ===== */
-  if (statusStep === 0) {
-    console.log("[inputData] STEP 1");
-    inputDataUmum();
+  /* ========= JFT ========= */
+  if (exam === "F10-E10J") {
+    var rLang = document.querySelector('input[name="rdoLang"]');
+    if (rLang) rLang.click();
 
-    /* ========= JFT ========= */
-    if (exam === "F10-E10J") {
-      var rLang = document.querySelector('input[name="rdoLang"]');
-      if (rLang) rLang.click();
+    var selLang = document.querySelector('select[name="selLang"]');
+    if (selLang) selLang.value = "Indonesian";
 
-      var selLang = document.querySelector('select[name="selLang"]');
-      if (selLang) selLang.value = "Indonesian";
+    /* checkbox khusus JFT */
+    var occ = document.querySelector('input[name="chkOccupation"][value="M"]');
+    if (occ) occ.click();
 
-      /* checkbox khusus JFT */
-      var occ = document.querySelector('input[name="chkOccupation"][value="M"]');
-      if (occ) occ.click();
+    var cbt = document.querySelector('input[name="chkCBT"][value="F"]');
+    if (cbt) cbt.click();
 
-      var cbt = document.querySelector('input[name="chkCBT"][value="F"]');
-      if (cbt) cbt.click();
+    var tb = document.querySelector('input[name="chkTextbook"][value="D"]');
+    if (tb) tb.click();
 
-      var tb = document.querySelector('input[name="chkTextbook"][value="D"]');
-      if (tb) tb.click();
+    var ws = document.querySelector('input[name="chkWebSite"][value="A"]');
+    if (ws) ws.click();
 
-      var ws = document.querySelector('input[name="chkWebSite"][value="A"]');
-      if (ws) ws.click();
+    var tr = document.querySelector('select[name="selTraveling"]');
+    if (tr) tr.value = "No, I have not been to Japan before";
 
-      var tr = document.querySelector('select[name="selTraveling"]');
-      if (tr) tr.value = "No, I have not been to Japan before";
+    var st = document.querySelector('select[name="selStudy"]');
+    if (st) st.value = "Over 300 hours";
 
-      var st = document.querySelector('select[name="selStudy"]');
-      if (st) st.value = "Over 300 hours";
+    var ss = document.querySelector('select[name="selStatus"]');
+    if (ss) ss.value = "A";
+  } else if (exam === "T20-J11J" || exam === "T10-J11J") {
+    /* ========= SSW FOOD ========= */
+    var job = document.querySelector('select[name="selJob"]');
+    if (job) job.value = "University student/graduate student";
 
-      var ss = document.querySelector('select[name="selStatus"]');
-      if (ss) ss.value = "A";
-    } else if (exam === "T20-J11J" || exam === "T10-J11J") {
-      /* ========= SSW FOOD ========= */
-      var job = document.querySelector('select[name="selJob"]');
-      if (job) job.value = "University student/graduate student";
+    var r1 = document.querySelector('input[name="chkResidence"][value="A"]');
+    if (r1) r1.click();
 
-      var r1 = document.querySelector('input[name="chkResidence"][value="A"]');
-      if (r1) r1.click();
+    var r2 = document.querySelector('input[name="chkWork"][value="A"]');
+    if (r2) r2.click();
 
-      var r2 = document.querySelector('input[name="chkWork"][value="A"]');
-      if (r2) r2.click();
+    var rt = document.querySelector('input[name="rdoTaken"][value="This is the first time."]');
+    if (rt) rt.click();
 
-      var rt = document.querySelector('input[name="rdoTaken"][value="This is the first time."]');
-      if (rt) rt.click();
-
-      var sl = document.querySelector('select[name="selLearn"]');
-      if (sl) {
-        sl.value = "I knew that there were learning texts, but I didn't know where I could find them.";
-      }
-
-      var kn = document.querySelector('input[name="chkKnows"][value="A"]');
-      if (kn) kn.click();
-
-      var ab = document.querySelector('input[name="rdoAbility"][value="Have passed"]');
-      if (ab) ab.click();
-    } else if (exam === "JH0-I11J" || exam === "JH0-I12J" || exam === "JH0-J12J") {
-      /* ========= KAIGO ========= */
-      var ac = document.querySelector('select[name="selAcademic"]');
-      if (ac) ac.value = "High school graduate";
-
-      var ex = document.querySelector('select[name="SelExp"]');
-      if (ex) ex.value = "I don't have any work experience.";
-
-      var vi = document.querySelector('select[name="selVisit"]');
-      if (vi) vi.value = "No, I have not been to Japan before";
-
-      var n1 = document.querySelector('select[name="selNursing1"]');
-      if (n1) n1.value = "less than 1 month";
-
-      var n2 = document.querySelector('select[name="selNursing2"]');
-      if (n2) n2.value = "self study";
-
-      var lv = document.querySelector('select[name="selJpLevel"]');
-      if (lv) lv.value = "JFT-Basic";
-    } else if (exam === "NC0-I11J" || exam === "NC0-I12J") {
-      /* ========= PERTANIAN ========= */
-      var trv = document.querySelector('select[name="selTraveling"]');
-      if (trv) trv.value = "No";
-
-      var st2 = document.querySelector('select[name="selStudy"]');
-      if (st2) st2.value = "80 hours or less";
-
-      var en = document.querySelector('select[name="selEng"]');
-      if (en) en.value = "None";
-
-      var ag = document.querySelector('select[name="selAgre"]');
-      if (ag) ag.value = "Agree";
-
-      var ss2 = document.querySelector('select[name="selStatus"]');
-      if (ss2) ss2.value = "I will not take the test in Japan.";
+    var sl = document.querySelector('select[name="selLearn"]');
+    if (sl) {
+      sl.value = "I knew that there were learning texts, but I didn't know where I could find them.";
     }
 
-    localStorage.setItem(STATUS_STEP_KEY, "1");
-    retryClickNext(10);
-    return;
-  }
+    var kn = document.querySelector('input[name="chkKnows"][value="A"]');
+    if (kn) kn.click();
 
-  /* ===== STEP 2 ===== */
-  if (statusStep === 1) {
-    console.log("[inputData] STEP 2");
-    localStorage.removeItem(STATUS_STEP_KEY);
-    retryClickNext(10);
-    return;
+    var ab = document.querySelector('input[name="rdoAbility"][value="Have passed"]');
+    if (ab) ab.click();
+  } else if (exam === "JH0-I11J" || exam === "JH0-I12J" || exam === "JH0-J12J") {
+    /* ========= KAIGO ========= */
+    var ac = document.querySelector('select[name="selAcademic"]');
+    if (ac) ac.value = "High school graduate";
+
+    var ex = document.querySelector('select[name="SelExp"]');
+    if (ex) ex.value = "I don't have any work experience.";
+
+    var vi = document.querySelector('select[name="selVisit"]');
+    if (vi) vi.value = "No, I have not been to Japan before";
+
+    var n1 = document.querySelector('select[name="selNursing1"]');
+    if (n1) n1.value = "less than 1 month";
+
+    var n2 = document.querySelector('select[name="selNursing2"]');
+    if (n2) n2.value = "self study";
+
+    var lv = document.querySelector('select[name="selJpLevel"]');
+    if (lv) lv.value = "JFT-Basic";
+  } else if (exam === "NC0-I11J" || exam === "NC0-I12J") {
+    /* ========= PERTANIAN ========= */
+    var trv = document.querySelector('select[name="selTraveling"]');
+    if (trv) trv.value = "No";
+
+    var st2 = document.querySelector('select[name="selStudy"]');
+    if (st2) st2.value = "80 hours or less";
+
+    var en = document.querySelector('select[name="selEng"]');
+    if (en) en.value = "None";
+
+    var ag = document.querySelector('select[name="selAgre"]');
+    if (ag) ag.value = "Agree";
+
+    var ss2 = document.querySelector('select[name="selStatus"]');
+    if (ss2) ss2.value = "I will not take the test in Japan.";
   }
-  console.log("[inputData] STEP >= 2, no action");
 })();
